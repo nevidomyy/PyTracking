3399f4f8ac7531172befba0c2ac30d6183b4f5b2
Nevidomyy Igor <69916631+nevidomyy@users.noreply.github.com>
2020-10-30 23:54:17 +0300

    Delete options.py

--- options.py
+++
@@ -1,8 +0,0 @@
-My_Host = 'localhost'
-My_User = 'TrackUser'
-My_Password = 'Track123@'
-My_DB_name = 'vh221808_aifun'
-
-headers = {
-    'X-Authorization-Token': 'fd0199ae570d0f68101675aef2eedeae9b56d9fc4217a771e669ba2e53cc43056956fcd22eb62687'
-    }
