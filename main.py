d8876ef0fc1c7244696aaf0330d47ad24e1a2382
Igor <nevidom4ik@list.ru>
2020-11-02 19:48:16 +0300

    # pro code

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 import json
-import mysql.connector
 from mysql.connector import Error
+from mysql.connector import connect
 import time
 import options
 
 
-def create_connection(host_name, user_name, user_password, db_name):
+def create_connection(host_name: str, user_name: str, user_password: str, db_name: str) -> connect:
     connection = None
     try:
-        connection = mysql.connector.connect(
+        connection = connect(
             host=host_name,
             user=user_name,
             passwd=user_password,
@@ -31,7 +31,11 @@ def jprint(obj):
 
 
 def tracking(track):
-    # define the delivery service
+    """
+    :define: delivery service and get track info
+    :param track: Track number from BD
+    :return: json track info
+    """
     response = requests.get('https://gdeposylka.ru/api/v4/tracker/detect/' + track, headers=options.headers)
     if response.status_code == 200:
         answer = response.json()
@@ -53,16 +57,17 @@ def tracking(track):
 
 # contains conditions for selecting track numbers
 def get_track_numbers():
-    connect = create_connection(options.My_Host, options.My_User, options.My_Password, options.My_DB_name)
-    query = connect.cursor()
+    connection = create_connection(options.My_Host, options.My_User, options.My_Password, options.My_DB_name)
+    query = connection.cursor()
     query.execute("SELECT ID, Trackcode FROM aop_rsticketspro_ticket_notes WHERE ID > "
                   "(SELECT LastProcessedID FROM StartIndex)")
     query_result = query.fetchall()
     return query_result
 
 
-def parsing(track):
-    jprint(track)
+def parsing(trackinfo):
+    # ToDo: array parsing to database from json track info
+    jprint(trackinfo)
     return
 
 
@@ -71,7 +76,7 @@ row, number = 0, 0
 # in range(count) count - the number of processed tracks per run
 for number in range(options.track_count):
     if number < len(results):
-        print(str(row+1) + '. ' + 'Обработка трек-номера: ' + str(results[row][0]) + ' ' + str(results[row][1]))
+        print(f'{row + 1}. Обработка трек-номера: {results[row][0]}{results[row][1]}')
         TrackNumber = results[row][1]
         # start post tracking function and writing result into JSAnswer
         JSAnswer = tracking(TrackNumber)
