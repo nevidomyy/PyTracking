8cecc0bdc347097ee39bb6943c07ff2f0c8843ab
Igor <nevidom4ik@list.ru>
2020-11-21 13:51:30 +0300

    # added QUIT() command if i don't have connection

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ def create_connection(host_name: str, user_name: str, user_password: str, db_nam
         # print("Connection to MySQL DB successful")
     except Error as e:
         print(f"The error '{e}' occurred")
-        print(f'ОШИБКА... Выполнение программы остановлено! Отсутствует подключение к БД.')
-        quit()
+        quit(f'ОШИБКА... Выполнение программы остановлено! Отсутствует подключение к БД.')
 
     return connection
 
