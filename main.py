92691830a17947392d4d4277e21ef5d72d52e54d
Nevidomyy Igor <69916631+nevidomyy@users.noreply.github.com>
2020-11-26 15:40:36 +0300

    # added except timeout

--- main.py
+++ main.py
@@ -52,16 +52,22 @@ def tracking(track: str) -> json:
     :return: json track info
     """
     time.sleep(2)
-    response = requests.get(f'https://gdeposylka.ru/api/v4/tracker/detect/{track}', headers=options.headers, timeout=30)
+    try:
+        response = requests.get(f'https://gdeposylka.ru/api/v4/tracker/detect/{track}', headers=options.headers, timeout=30)
+    except requests.Timeout:
+        logging.info("OOPS!! Timeout Error")    
     if response.status_code == 200:
         answer = response.json()
         # if result of detecting delivery service is successful
         if answer['result'] == 'success':
-            slug = answer['data'][0]['courier']['slug']
-            # getting info for track
+            slug = answer['data'][0]['courier']['slug']           
+            # getting info for track            
             time.sleep(2)
-            response = requests.get(f'https://gdeposylka.ru/api/v4/tracker/{slug}/{track}',
+            try:
+                response = requests.get(f'https://gdeposylka.ru/api/v4/tracker/{slug}/{track}',
                                     headers=options.headers, timeout=30)
+            except requests.Timeout:
+                logging.info("OOPS!! Timeout Error") 
             if response.status_code == 200:
                 answer = response.json()
                 return answer
